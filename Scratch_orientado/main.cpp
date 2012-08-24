8e266b23688e15864a18f65baa9a8c5c4611eb10
Marco Antonio Mayen <markov.mayen@gmail.com>
2012-08-23 22:16:51 -0500

    Solo envia send con sizeof
    No se que ocurre solo envia cuando tiene sizeof quiza sea el tamaño de
    la trama aun no lo se u.u

--- Scratch_orientado/main.cpp
+++ Scratch_orientado/main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
     sprintf(buffer+4,"broadcast \"hola\"");
     *ap=htonl(strlen(buffer+4));
 
-    printf(" %x%x%x%x ",(int)buffer[0],(int)buffer[1],(int)buffer[2],(int)buffer[3]);
+    printf(" %x%x%x%x ",(int)buffer[0],(int)buffer[1],(int)buffer[2],buffer[3]);
     printf("%s",buffer+4);
 
 
@@ -51,7 +51,10 @@ int main(int argc, char *argv[])
     data.sin_addr.s_addr = inet_addr(ipdest);// PENDIENTE IPDESTINO
     if(connect(s,(const sockaddr*)&data,sizeof(data))!=0)
         perror("NO CONECTION ESTABLISHED");
-    if(send(s,buffer,strlen(buffer),0)==-1)
+    // NO SE SI NO ESTA REALIZANDO EL ENVIO YA QUE NO APARECE EN NC
+    if(send(s,buffer,strlen(buffer)+1,0)==-1) // PQ PARECE Q ESTE NO LO ENVIA Y EL OTRO SI ??
+        perror("EOOR NOT SEND");
+    if(send(s,buffer,sizeof(buffer)+1,0)==-1)
         perror("EOOR NOT SEND");
     if((recv(s,buffer_in,sizeof(buffer_in),0))!= -1)
         perror("ERROR AL RECIBIR");
